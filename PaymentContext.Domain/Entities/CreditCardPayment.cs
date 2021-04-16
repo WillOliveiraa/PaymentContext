2fc291165e1155b5826834e6ed44c62fb6a5b2e5
willian oliveira <willian.lemes@iatec.com>
2021-04-16 12:02:21 -0300

    Corrupção no código - 8

--- PaymentContext.Domain/Entities/CreditCardPayment.cs
+++ PaymentContext.Domain/Entities/CreditCardPayment.cs
@@ -1,38 +1,38 @@
-//using System;
+using System;
 //using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Domain.Entities
 {
     public class CreditCardPayment : Payment
     {
-        //public CreditCardPayment(
-        //    string cardHolderName, 
-        //    string cardNumber, 
-        //    string lastTransactionNumber, 
-        //    DateTime paidDate,
-        //    DateTime expireDate,
-        //    decimal total,
-        //    decimal totalPaid,
-        //    string payer,
-        //    Document document,
-        //    Address address, 
-        //    Email email) : base(
-        //        paidDate,
-        //        expireDate,
-        //        total,
-        //        totalPaid,
-        //        payer,
-        //        document,
-        //        address,
-        //        email)
-        //{
-        //    CardHolderName = cardHolderName;
-        //    CardNumber = cardNumber;
-        //    LastTransactionNumber = lastTransactionNumber;
-        //}
+        public CreditCardPayment(
+            string cardHolderName,
+            string cardNumber,
+            string lastTransactionNumber,
+            DateTime paidDate,
+            DateTime expireDate,
+            decimal total,
+            decimal totalPaid,
+            string payer,
+            string document,
+            string address,
+            string email) : base(
+                paidDate,
+                expireDate,
+                total,
+                totalPaid,
+                payer,
+                document,
+                address,
+                email)
+        {
+            CardHolderName = cardHolderName;
+            CardNumber = cardNumber;
+            LastTransactionNumber = lastTransactionNumber;
+        }
 
-        public string CardHolderName { get; set; }
-        public string CardNumber { get; set; }
-        public string LastTransactionNumber { get; set; }
+        public string CardHolderName { get; private set; }
+        public string CardNumber { get; private set; }
+        public string LastTransactionNumber { get; private set; }
     }
 }
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Willian", "Oliveira", "123456789", "will.oliveira93@gmail.com");
+            //student.FirstName = "";
+            //student.Subscriptions.Add(subscription);
+            student.AddSubscription(subscription);
+        }
+    }
+}
