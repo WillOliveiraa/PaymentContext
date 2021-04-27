9720199860bebcd22feadaa36e6977df2a0e7f5f
willian oliveira <willian.lemes@iatec.com>
2021-04-27 13:39:04 -0300

    Testando os Commands - 17

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Command;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        // Red, Green, Refactor
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+
+            command.Validate();
+            Assert.AreEqual(false, command.Valid);
+        }
+    }
+}
