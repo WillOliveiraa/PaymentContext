4153c4ca5d10c615cae38471045a5b08616ae857
willian oliveira <willian.lemes@iatec.com>
2021-04-28 11:24:16 -0300

    Testando os Handlers - 20

---
+++ PaymentContext.Tests/Handlers/SubscriptionHandlerTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Command;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Handlers;
+using PaymentContext.Tests.Mocks;
+using System;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class SubscriptionHandlerTest
+    {
+        // Red, Green, Refactor
+        [TestMethod]
+        public void ShouldReturnErrorWhenDocumentExists()
+        {
+            var handler = new SubscriptionHandler(new FakeStudentRepository(), new FakeEmailService());
+            var command = new CreateBoletoSubscriptionCommand();
+
+            command.FirstName = "Will";
+            command.LastName = "Oliveira";
+            command.Document = "99999999999";
+            command.Email = "willoliveira@teste.com";
+
+            command.BarCode = "123456789";
+            command.BoletoNumber = "12345679";
+
+            command.PaymentNumber = "2312564";
+            command.PaidDate = DateTime.Now;
+            command.ExpireDate = DateTime.Now.AddMonths(1);
+            command.Total = 60;
+            command.TotalPaid = 60;
+            command.Payer = "Will Corp";
+            command.PayerDocument = "132456498789";
+            command.PayerDocumentType = EDocumentType.CPF;
+            command.EmailPayer = "will@tes.tecom";
+            command.Street = "Rua da Moda";
+            command.Number = "123";
+            command.Neighborhood = "Centro";
+            command.City = "Maringá";
+            command.State = "PR";
+            command.Country = "Brazil";
+            command.ZipCode = "213156465";
+
+            handler.Handle(command);
+            Assert.AreEqual(false, handler.Valid);
+        }
+    }
+}
